43df6b3a1c073c9efe9aafa4a89c41edc717122b
krasseman <32762094+krasseman@users.noreply.github.com>
2018-03-05 15:34:18 +0100

    .

--- myhoroscope.php
+++ myhoroscope.php
@@ -1,21 +1,22 @@
 <?php
 include "classes.php";
 
-if(isset($_COOKIE["user"])) {
-    header('Location: ./myhoroscope.php');  
-} 
+
 
 $myPerson = new Person($_POST["fName"], $_POST["lName"], $_POST["bDate"]);
 setcookie("user", serialize($myPerson), time() + 120);
 
+if(isset($_COOKIE["user"])) {
+    
 
+ } 
 
 
 
 $myPerson -> printName();
 echo "</br>";
 $myPerson -> checkHoroscope();
-print_r($myPerson);
+
 echo "</br>";
 echo $_COOKIE["user"];
 
