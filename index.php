801e3453017ec4c2776cda9876de95062c3e7e1c
antonpetersson <antonpetersson@users.noreply.github.com>
2018-03-05 15:44:35 +0100

    småfix

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST")
 {
     $myPerson = new Person($_POST["fName"], $_POST["lName"], $_POST["bDate"]);
     setcookie("user", serialize($myPerson), time() + 7 * 24 * 60 * 60);
+    $_COOKIE["user"] = serialize($myPerson);
 }
 
 if(isset($_COOKIE["user"])) {
